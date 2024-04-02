6dcbd8643ccfbd843ace80524e43d2907b1b0605
itsaswinravi <achu322001@gmail.com>
2024-04-02 14:50:44 +0530

    16

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ app.include_router(vote .router)
 
 @app.get("/")
 def root():
-    return {"message": "welcome to my api"}
+    return {"message": "hllo aaa"}
 
 
 
---
+++ tests/test_users.py
@@ -0,0 +1,33 @@
+from app import schemas
+from .database import client,session
+import pytest
+from jose import jwt
+from app.config import settings
+
+
+# def test_root(client):
+   
+#     res = client.get("/")
+#     print(res.json().get('message'))
+#     assert res.json().get('message') == 'hllo aaa'
+#     assert res.status_code == 200
+
+
+def test_create_user(client):
+    res = client.post("/users/",json={"email":"ak123@gmail.com","password":"password123"})
+    
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "ak123@gmail.com"
+    assert res.status_code == 201
+
+def test_login_user( test_user,client):
+    res = client.post(
+        "/login" , data={"username": test_user['email'], "password": test_user['password']}
+    )
+
+    login_res = schemas.Token(**res.json())
+    payload= jwt.decode(login_res.access_token,settings.secret_key, algorithms=[settings.algorithm])
+    id= payload.get("user_id") 
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200 
